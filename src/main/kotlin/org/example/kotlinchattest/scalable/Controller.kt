e78a1caf308f15fb100ebdce94fadc6131533392
peter <magyarpeter222@gmail.com>
2026-04-15 00:11:50 +0200

    implement response

--- src/main/kotlin/org/example/kotlinchattest/scalable/Controller.kt
+++ src/main/kotlin/org/example/kotlinchattest/scalable/Controller.kt
@@ -3,6 +3,7 @@ package org.example.kotlinchattest.scalable
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
 
 @ConditionalOnProperty(name=["common.mode"], havingValue="scalable")
@@ -11,8 +12,9 @@ class Controller(
     private val rabbitTemplate: RabbitTemplate
 ) {
     @MessageMapping("/hello")
-    fun greeting(message: Message) {
+    fun greeting(message: Message): String {
         println("Received message from client: $message")
         rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_NAME, "chat", message)
+        return "hello"
     }
 }
--- src/main/kotlin/org/example/kotlinchattest/scalable/Listener.kt
+++ src/main/kotlin/org/example/kotlinchattest/scalable/Listener.kt
@@ -1,12 +1,16 @@
 package org.example.kotlinchattest.scalable
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class Listener {
+class Listener(
+    private val messagingTemplate: SimpMessagingTemplate
+) {
     @RabbitListener(queues = [RabbitConfig.QUEUE_NAME])
-    fun onMessage(message: Message){
+    fun onMessage(message: Message) {
         println("Received message from queue: $message")
+        messagingTemplate.convertAndSend("/topic/hello", message)
     }
 }
